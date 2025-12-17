12dbc1ccaf8af3859aa87c54257c7967ef73ada1
ReA7LeGeNd <noureddine.choukhairi@emsi-edu.ma>
2025-12-17 16:53:13 +0100

    Statistique added and syntax errors fixed

--- src/main/java/ma/TeethCare/repository/api/situationFinanciereRepository.java
+++ src/main/java/ma/TeethCare/repository/api/situationFinanciereRepository.java
@@ -4,5 +4,5 @@ import ma.TeethCare.entities.situationFinanciere.situationFinanciere;
 import ma.TeethCare.repository.common.CrudRepository;
 
 public interface SituationFinanciereRepository extends CrudRepository<situationFinanciere, Long> {
-
+//problem de renomation reglé
 }
